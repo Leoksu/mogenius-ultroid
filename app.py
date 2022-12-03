d3feafed73865e2e098f3d63e5f25d4c85920a2a
Leoksu <118695013+Leoksu@users.noreply.github.com>
2022-12-03 17:24:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-#get your github token in https://github.com/settings/tokens
 
 #Ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
+
 os.system("git clone https://username:token@github.com/username/reponame && cd Ultroid && pip install -r requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m pyUltroid &")
