85e9e9fb6ad3f1daaed2c9f9115c1e3099b2d6af
Leoksu <118695013+Leoksu@users.noreply.github.com>
2022-12-03 18:13:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def hello_world():
    return 'Hello, World!'
 
 
-#Ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
+#ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
+#note : change reponame with your Ultroid fork repo and don't forget to create .env file with all filled vars 
 
 os.system("git clone https://username:token@github.com/username/reponame && cd Ultroid && pip install -r requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m pyUltroid &")
