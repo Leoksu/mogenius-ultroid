0e0f82b655b82fd36201673418003e27ff812217
Leoksu <leoksuck@gmail.com>
2022-12-06 14:16:43 +0700

    Update app.py
    Fixing command

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ def hello_world():
 #ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
 
 
-os.system("git clone https://username:token@github.com/username/Ultroid && cd Ultroid && pip install -r requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m pyUltroid &
-")
+os.system("git clone https://username:token@github.com/username/Ultroid && cd Ultroid && pip install -r requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m pyUltroid &")
