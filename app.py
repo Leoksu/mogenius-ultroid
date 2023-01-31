732a7c2bfcd1bd30d7c3c48a8900426e3f1fc730
eleven <leoksuck@gmail.com>
2023-02-01 05:55:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://github.com/TeamUltroid/Ultroid && cd Ultroid && pip3 install -r requirements.txt && pip3 install -r re*/st*/op* && curl -s -O https://$GIT_TOKEN@raw.githubusercontent.com/$GIT_URL && python3 -m pyUltroid &")
+os.system("git clone https://github.com/TeamUltroid/Ultroid && cd Ultroid && pip3 install -r requirements.txt && pip3 install -r re*/st*/op* && cp ../.env .env && bash startup &")
